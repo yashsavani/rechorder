1358b0eddd1f518ff5d1c001c47ac825251446ef
yashsavani <yash.savani@gmail.com>
2014-11-15 18:17:59 -0800

    added a kmeans cluter evaluation function.

--- chordKMeans.py
+++ chordKMeans.py
@@ -10,6 +10,12 @@ def getBestBarList(midiFileName):
 	return barLists[0]
 	#return best bar list in barLists
 
+# gets a list of euclidean distances between the rows of mat_a and mat_b[index]
+def euclideanDistance(mat_a, mat_b, index) :
+	diff_mat = np.subtract(mat_a, np.array([mat_b[index] for _ in range(mat_a.shape[0])]))
+	dists = [np.linalg.norm(vec) for vec in diff_mat]
+	return dists
+
 def getFeatureCentroids(midiFileName, numCentroids=12, maxIterations=100): # basically k-means
 	bestBarList = getBestBarList(midiFileName)
 	numExamples = len(bestBarList)
@@ -30,9 +36,7 @@ def getFeatureCentroids(midiFileName, numCentroids=12, maxIterations=100): # bas
 		new_corr_centers = []
 		# Find closest cluster centers for each point
 		for index in range(numExamples) :
-			# Euclidean Distance
-			diff_mat = np.subtract(centroids_mat, np.array([data_mat[index] for _ in range(numCentroids)]))
-			dists = [np.linalg.norm(vec) for vec in diff_mat]
+			dists = euclideanDistance(centroids_mat, data_mat, index)
 			center = np.argmin(dists)
 			new_corr_centers.append(center)
 			corr_points[center].append(index)
@@ -48,4 +52,23 @@ def getFeatureCentroids(midiFileName, numCentroids=12, maxIterations=100): # bas
 		corr_centers = list(new_corr_centers)
 
 	return (centroids_mat, corr_centers)
+
+def evaluateKmeansClusters(midiFileName, centroids, corr_centers) :
+	bestBarList = getBestBarList(midiFileName)
+	numExamples = len(bestBarList)
+	numCentroids = centroids.shape[0]
+
+	# parse bars into a data matrix
+	data_mat = np.array([bar.getKMeansFeatures() for bar in bestBarList])
+
+	def silhouette(index) :
+		same_cntr_pts = data_mat[[i for i, x in enumerate(corr_centers) if x == corr_centers[index]]]
+		a_i = np.mean(euclideanDistance(same_cntr_pts, data_mat, index))
+
+		diff_centroids = centroids[[i for i in range(numCentroids) if i != corr_centers[index]]]
+		b_i = np.min(euclideanDistance(diff_centroids, data_mat, index))
+
+		return (b_i - a_i) / max(a_i, b_i)
+
+	return np.mean([silhouette(i) for i in range(numExamples)])
 
