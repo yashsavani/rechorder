a2757cf3f535e7ce60e466f15a1679de3e0d54af
Wilbur Yang <wily44@gmail.com>
2014-11-15 21:36:03 -0800

    Fixed closest centroids / multiple file input.

--- visualizer.py
+++ visualizer.py
@@ -14,70 +14,67 @@ if len(sys.argv) == 1:
 else:
   midiFiles = sys.argv[1:]
 
-barLists = util.getNGramBarList(midiFiles, n=4)
-'''
-for x in barLists:
-  for y in x:
-    print y
-'''
+for midiFile in midiFiles:
+  barLists = util.getNGramBarList(midiFile, n=4)
+  '''
+  for x in barLists:
+    for y in x:
+      print y
+  '''
 
-featureCentroids, centroidAssignments = chordKMeans.getFeatureCentroids(midiFiles)
+  featureCentroids, centroidPoints = chordKMeans.getFeatureCentroids(midiFiles)
 
-bestBarList = barLists[0]
-print len(bestBarList), bestBarList
-totalBars = len(bestBarList)
+  bestBarList = barLists[0]
+  print len(bestBarList), bestBarList
+  totalBars = len(bestBarList)
 
-print featureCentroids[0]
+  print featureCentroids[0]
 
-colors = list('bgrcmyk')
-color = {}
-print totalBars
-print centroidAssignments
-for i, bar in enumerate(bestBarList):
-  closestCentroid = chordKMeans.getClosestCentroid(featureCentroids, bar.getKMeansFeatures(), 0)
-  closestCentroid = centroidAssignments[i]
-  if len(colors) > 0:
-    if closestCentroid not in color:
-      color[closestCentroid] = colors.pop(0)
-  else:
-    color[closestCentroid] = 'r'
+  colors = list('bgrcmyk')
+  color = {}
+  print totalBars
+  print centroidAssignments
+  for i, bar in enumerate(bestBarList):
+    closestCentroid = chordKMeans.getClosestCentroid(featureCentroids, bar.getKMeansFeatures(), 0)
+    if len(colors) > 0:
+      if closestCentroid not in color:
+        color[closestCentroid] = colors.pop(0)
+    else:
+      color[closestCentroid] = 'r'
 
-print color
+  print color
 
-for i, bar in enumerate(bestBarList):
-  closestCentroid = chordKMeans.getClosestCentroid(featureCentroids, bar.getKMeansFeatures(), 0)
-
-  #closestCentroid2 is supposed to equal closestCentroid, but it doesn't
-  closestCentroid = centroidAssignments[i]
-  
-  #x = np.linspace(0 - (i - 0.5) / float(totalBars), (i + 0.5) / float(totalBars) + 100, 2)
-  x = np.array([i / float(totalBars), i / float(totalBars), (i + 1) / float(totalBars), (i + 1) / float(totalBars)])
-  y = np.array([200] * 4)
-  y[0] = 0
-  y[3] = 0
-  p = plt.fill(x, y, color[closestCentroid], alpha=0.2)
-  #plt.grid(True)
+  for i, bar in enumerate(bestBarList):
+    closestCentroid = chordKMeans.getClosestCentroid(featureCentroids, bar.getKMeansFeatures(), 0)
+    
+    #x = np.linspace(0 - (i - 0.5) / float(totalBars), (i + 0.5) / float(totalBars) + 100, 2)
+    x = np.array([i / float(totalBars), i / float(totalBars), (i + 1) / float(totalBars), (i + 1) / float(totalBars)])
+    y = np.array([200] * 4)
+    y[0] = 0
+    y[3] = 0
+    p = plt.fill(x, y, color[closestCentroid], alpha=0.2)
+    #plt.grid(True)
 
 
 
-for bar in bestBarList:
-  pass #print bar
+  for bar in bestBarList:
+    pass #print bar
 
-# plot each beat individually
-for i, bar in enumerate(bestBarList):
-  for beat in bar.beats:
-    for note, duration in beat:
-      x = np.linspace(i / float(totalBars), (i + 1) / float(totalBars), 2)
-      y = np.zeros(2)
-      y[0] = note
-      y[1] = note
-      p = plt.plot(x, y, 'r')
-      #print duration
-      pylab.setp(p, linewidth = duration * 5)
+  # plot each beat individually
+  for i, bar in enumerate(bestBarList):
+    for beat in bar.beats:
+      for note, duration in beat:
+        x = np.linspace(i / float(totalBars), (i + 1) / float(totalBars), 2)
+        y = np.zeros(2)
+        y[0] = note
+        y[1] = note
+        p = plt.plot(x, y, 'r')
+        #print duration
+        pylab.setp(p, linewidth = duration * 5)
 
-plt.axis([0, 1, 0, util.NUM_NOTES])
-plt.show()
+  plt.axis([0, 1, 0, util.NUM_NOTES])
+  plt.show()
 
-# part 2... hopefully we'll get here
+  # part 2... hopefully we'll get here
 
 
