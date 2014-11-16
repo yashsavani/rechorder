c578dfac1f8c4cd0b2de5d6dc80ae30d73538b9f
yashsavani <yash.savani@gmail.com>
2014-11-15 17:24:44 -0800

    k means initial implementation complete, now returns (clusters, assignments of examples to clusters.)

--- chordKMeans.py
+++ chordKMeans.py
@@ -3,46 +3,49 @@ import util
 import numpy as np
 import random
 
-np.set_printoptions(formatter={'float': lambda x: str(x)+ '\t'})
+np.set_printoptions(formatter={'float': lambda x: '%.2f\t'%round(x,2)})
 
 def getBestBarList(midiFileName):
 	barLists = util.getNGramBarList(midiFileName)
+	return barLists[0]
 	#return best bar list in barLists
 
 def getFeatureCentroids(midiFileName, numCentroids=12, maxIterations=100): # basically k-means
 	bestBarList = getBestBarList(midiFileName)
+	numExamples = len(bestBarList)
 
 	# parse bars into a data matrix
-	data_mat = np.array([bar.getKMeansFeatures() for bar in bestBarList]).transpose()
+	data_mat = np.array([bar.getKMeansFeatures() for bar in bestBarList])
 
 	# initialize the k clusters from k randomly chosen points in the data
-	indices = range(data_mat.shape[1])
+	indices = range(numExamples)
 	random.shuffle(indices)
-	centroids_mat = data_mat[:,indices[:numCentroids]]
+	centroids_mat = data_mat[indices[:numCentroids]]
 
 	iterations = 0
-	corr_centers = [-1]*data_mat.shape[1]
-	for _ in maxIterations :
+	corr_centers = [-1]*numExamples
+	for _ in range(maxIterations) :
 		iterations += 1
-		corr_points = [[] for _ in range(centroids_mat.shape[1])]
+		corr_points = [[] for _ in range(numCentroids)]
 		new_corr_centers = []
 		# Find closest cluster centers for each point
-		for index in range(data_mat.shape[1]) :
-			diff_mat = np.subtract(centroids_mat, np.array([data_mat[:,index] for _ in range(centroids_mat.shape[1])]).transpose()).transpose()
+		for index in range(numExamples) :
+			# Euclidean Distance
+			diff_mat = np.subtract(centroids_mat, np.array([data_mat[index] for _ in range(numCentroids)]))
 			dists = [np.linalg.norm(vec) for vec in diff_mat]
 			center = np.argmin(dists)
 			new_corr_centers.append(center)
 			corr_points[center].append(index)
 
 		# Move cluster center to center of corresponding points
-		for index in range(centroids_mat.shape[1]) :
-			rel_points = data_mat[:,corr_points[index]]
-			centroids_mat[:,index] = np.array([np.mean(pt_points) for pt_points in rel_points])
+		for index in range(numCentroids) :
+			rel_points = data_mat[corr_points[index]].transpose()
+			centroids_mat[index] = np.array([np.mean(pt_points) if pt_points.any() else 0 for pt_points in rel_points])
 
 		if new_corr_centers == corr_centers :
 			break
 
 		corr_centers = list(new_corr_centers)
 
-	return centroids_mat
+	return (centroids_mat, corr_centers)
 
--- predictor.py
+++ predictor.py
@@ -3,6 +3,9 @@
 import util
 import chordKMeans
 import sys
+import numpy as np
+
+np.set_printoptions(formatter={'float': lambda x: '%.2f\t'%x})
 
 # part 1
 
@@ -16,7 +19,9 @@ for x in barLists:
   for y in x:
     print y.getKMeansFeatures()
 
-#featureCentroids = chordKMeans.getFeatureCentroids(midiFileName)
+featureCentroids = chordKMeans.getFeatureCentroids(midiFileName, 12)
+print featureCentroids[0]
+print featureCentroids[1]
 
 # part 2... hopefully we'll get here
 
